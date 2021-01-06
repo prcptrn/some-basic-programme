95a6af4fd5c692ad652b445bb76d7587d9573fdb
prcptrn <76002004+prcptrn@users.noreply.github.com>
2021-01-06 16:37:35 +0100

    Update rock-paper-scissors.py

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,47 +1,37 @@
-from random import randint
+from random import choice
 
-rps_value = 0
+
+def check(playera):
+    global rps
+    global usr
+    for el in rps:
+        if playera == el['name']:
+            usr = el
+            return True
+    return False
+
+
 player_move = True
+r = {'name': 'rock', 'win': ['scissors']}
+p = {'name': 'paper', 'win': ['rock']}
+s = {'name': 'scissors', 'win': ['paper']}
+rps = [r, p, s]
 
 while player_move:
     player_move = False
-    rps = str(input("choose rock, paper or scissors: "))
-    if rps == "rock":
-        rps_value = 1
-    elif rps == "paper":
-        rps_value = 2
-    elif rps == "scissors":
-        rps_value = 3
-    else:
+    player = input("chose rock, paper or scissors: ")
+    if not check(player):
+        print("not ok")
         player_move = True
 
-computer_rps = randint(1, 3)
+computer = choice(rps)
 
-if computer_rps == 1:
-    print("computer chose rock")
-    if rps_value == 1:
-        score = "tied"
-    elif rps_value == 2:
-        score = "won"
-    elif rps_value == 3:
-        score = "lost"
-
-elif computer_rps == 2:
-    print("computer chose paper")
-    if rps_value == 1:
-        score = "lost"
-    elif rps_value == 2:
-        score = "tied"
-    elif rps_value == 3:
-        score = "won"
-
-elif computer_rps == 3:
-    print("computer chose scissors")
-    if rps_value == 1:
-        score = "won"
-    elif rps_value == 2:
-        score = "lost"
-    elif rps_value == 3:
-        score = "tied"
-
-print("You have " + score)
+if computer['name'] == player:
+    print('computer has chosen ' + computer['name'])
+    print('The result is draw')
+elif computer['name'] in usr['win']:
+    print('computer has chosen ' + computer['name'])
+    print("You won")
+else:
+    print('computer has chosen ' + computer['name'])
+    print("You lost")
